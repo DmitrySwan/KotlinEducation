9fdb41a3896f243fb8c629aaee18be70a080db24
Дмитрий <lebedev.dmitry.swan@yandex.ru>
2021-04-21 21:11:27 +0400

    init

---
+++ src/Tasks.kt
@@ -0,0 +1,40 @@
+fun main() {
+    println(
+        repeatedIntersection(intArrayOf(3, 6, 7, 8, 7, 9), mutableListOf(12, 6, 7, 6, 7, 565))
+    )
+    println(countLetters("AAGGDDDFBBBAAABB"))
+    println(groupWords(arrayOf("ate", "tdb", "eat", "ref", "fer", "test")))
+}
+
+fun repeatedIntersection(array1: IntArray, array2: MutableList<Int>): List<Int> {
+    return array1
+        .filter { e ->
+            array2.remove(e)
+        }
+}
+
+fun countLetters(input: String): String {
+    var i = 0
+    var count = 0
+    var result = ""
+    for (letter in input) {
+        count++
+        i++
+        if (i < input.length && letter == input[i]) {
+            continue
+        }
+        result += "${input[i - 1]}$count"
+        count = 0
+    }
+    return result
+}
+
+fun groupWords(words: Array<String>): List<List<String>> {
+    val map: MutableMap<String, MutableList<String>> = HashMap()
+    for (word in words) {
+        val sum: String = word.chars().sorted().toArray().joinToString()
+        map.getOrPut(sum) { mutableListOf() }
+            .add(word)
+    }
+    return map.values.toList()
+}
